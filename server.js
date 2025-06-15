0cf421ce3c3c6c950c826224d9e5f5d200b1c080
rogo! <r480github@gmail.com>
2025-06-15 01:15:10 -0400

    chemical config

--- server.js
+++ server.js
@@ -2,28 +2,27 @@ import { ChemicalServer } from "chemicaljs";
 import express from "express";
 
 const [app, listen] = new ChemicalServer({
-    default: "scramjet", 
-    uv: "true",
-    scramjet: "true",
-    rh: "false",
+  uv: "true",
+  scramjet: "true",
+  rh: "true",
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
-app.use(express.static("public", {
+app.use(
+  express.static("public", {
     index: "index.html",
-    extensions: ["html"]
-}));
-
+    extensions: ["html"],
+  })
+);
 
 app.serveChemical();
 
-
 listen(port, () => {
-    console.log("░██████╗░░█████╗░██╗░░░░░░█████╗░██╗░░██╗██╗░░░██╗");
-    console.log("██╔════╝░██╔══██╗██║░░░░░██╔══██╗╚██╗██╔╝╚██╗░██╔╝");
-    console.log("██║░░╚██╗██╔══██║██║░░░░░██╔══██║░██╔██╗░░░╚██╔╝░░");
-    console.log("╚██████╔╝██║░░██║███████╗██║░░██║██╔╝╚██╗░░░██║░░░");
-    console.log("░╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░");
-    console.log("is listening on:");
-    console.log(`http://localhost:${port}`);
+  console.log("░██████╗░░█████╗░██╗░░░░░░█████╗░██╗░░██╗██╗░░░██╗");
+  console.log("██╔════╝░██╔══██╗██║░░░░░██╔══██╗╚██╗██╔╝╚██╗░██╔╝");
+  console.log("██║░░╚██╗██╔══██║██║░░░░░██╔══██║░██╔██╗░░░╚██╔╝░░");
+  console.log("╚██████╔╝██║░░██║███████╗██║░░██║██╔╝╚██╗░░░██║░░░");
+  console.log("░╚═════╝░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░");
+  console.log("is listening on:");
+  console.log(`http://localhost:${port}`);
 });
