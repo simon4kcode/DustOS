9721b67a6658752361837c24df0f48d55e80945d
rogo! <r480github@gmail.com>
2025-06-17 00:19:27 -0400

    some css fixes

---
+++ public/browser/animation.js
@@ -0,0 +1,13 @@
+  gsap.fromTo(
+    ".GalaxyBrowserh1",
+    {
+      opacity: 0,
+      y: "50vh",
+    },
+    {
+      opacity: 1,
+      y: "30vh",
+      duration: 1.7,
+      ease: "power2.inOut",
+    }
+  );
